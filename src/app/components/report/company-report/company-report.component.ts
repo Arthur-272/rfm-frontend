93c5165d5a8e47d8da899596682b44e133073c26
Radhey Patel <radheypatel272@gmail.com>
2024-12-07 01:00:57 -0400

    Date report set correctly from frontend.

--- src/app/components/report/company-report/company-report.component.ts
+++ src/app/components/report/company-report/company-report.component.ts
@@ -46,9 +46,19 @@ export class CompanyReportComponent implements OnInit {
    * Fetches report data based on the selected date.
    */
   fetchReportData(): void {
-    const dateToSend = this.selectedDate
-      ? this.adjustToBrowserTimezone(this.selectedDate)
-      : this.adjustToBrowserTimezone(new Date());
+
+    let current = new Date();
+
+    // Check current hour
+    if (current.getHours() < 4) {
+      // If it's before 4 AM, subtract one day
+      current.setDate(current.getDate() - 1);
+    }
+
+    // Set time to 4:00 AM
+    current.setHours(4, 0, 0, 0);
+
+    const dateToSend = this.adjustToBrowserTimezone(current);
 
     this.reportService.getReportData(dateToSend).subscribe({
       next: (data: ICatch[]) => {
